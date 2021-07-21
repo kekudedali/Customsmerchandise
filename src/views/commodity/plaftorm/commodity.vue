085f67bbe7940eac986af079baf56dcc7a3a9dd7
luli <luli@op-mobile.com.cn>
2021-07-21 23:13:02 +0800

    修改商品页面

--- src/views/commodity/plaftorm/commodity.vue
+++ src/views/commodity/plaftorm/commodity.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="app-container">
     <div class="radio-box">
-      <el-radio-group v-model="type" @change="changeStatus" size="small">
-        <el-radio-button class="daiban" :label="'0'">海关商品备案 </el-radio-button>
+      <el-radio-group
+        v-model="queryParams.type"
+        @change="changeStatus"
+        size="small"
+      >
+        <el-radio-button :label="'0'">海关商品备案 </el-radio-button>
         <el-radio-button :label="'1'">审核中</el-radio-button>
         <el-radio-button :label="'2'">审核驳回</el-radio-button>
         <el-radio-button :label="'3'">已归档</el-radio-button>
@@ -15,38 +19,14 @@
       v-show="showSearch"
       label-width="68px"
     >
-      <el-form-item label="公告标题" prop="noticeTitle">
+      <el-form-item label="商品名称" prop="noticeTitle">
         <el-input
           v-model="queryParams.noticeTitle"
-          placeholder="请输入公告标题"
+          placeholder="请输入商品名称"
           clearable
           size="small"
           @keyup.enter.native="handleQuery"
         />
-      </el-form-item>
-      <el-form-item label="操作人员" prop="createBy">
-        <el-input
-          v-model="queryParams.createBy"
-          placeholder="请输入操作人员"
-          clearable
-          size="small"
-          @keyup.enter.native="handleQuery"
-        />
-      </el-form-item>
-      <el-form-item label="类型" prop="noticeType">
-        <el-select
-          v-model="queryParams.noticeType"
-          placeholder="公告类型"
-          clearable
-          size="small"
-        >
-          <el-option
-            v-for="dict in typeOptions"
-            :key="dict.dictValue"
-            :label="dict.dictLabel"
-            :value="dict.dictValue"
-          />
-        </el-select>
       </el-form-item>
       <el-form-item>
         <el-button
@@ -70,32 +50,8 @@
           icon="el-icon-plus"
           size="mini"
           @click="handleAdd"
-          v-hasPermi="['system:notice:add']"
+          v-hasPermi="['commodity:plaftorm:add']"
           >新增</el-button
-        >
-      </el-col>
-      <el-col :span="1.5">
-        <el-button
-          type="success"
-          plain
-          icon="el-icon-edit"
-          size="mini"
-          :disabled="single"
-          @click="handleUpdate"
-          v-hasPermi="['system:notice:edit']"
-          >修改</el-button
-        >
-      </el-col>
-      <el-col :span="1.5">
-        <el-button
-          type="danger"
-          plain
-          icon="el-icon-delete"
-          size="mini"
-          :disabled="multiple"
-          @click="handleDelete"
-          v-hasPermi="['system:notice:remove']"
-          >删除</el-button
         >
       </el-col>
       <right-toolbar
@@ -104,66 +60,80 @@
       ></right-toolbar>
     </el-row>
 
-    <el-table
-      v-loading="loading"
-      :data="noticeList"
-      @selection-change="handleSelectionChange"
-    >
-      <el-table-column type="selection" width="55" align="center" />
-      <el-table-column
-        label="序号"
-        align="center"
-        prop="noticeId"
-        width="100"
-      />
-      <el-table-column
-        label="公告标题"
-        align="center"
-        prop="noticeTitle"
-        :show-overflow-tooltip="true"
-      />
-      <el-table-column
-        label="公告类型"
-        align="center"
-        prop="noticeType"
-        :formatter="typeFormat"
-        width="100"
-      />
-      <el-table-column
-        label="状态"
-        align="center"
-        prop="status"
-        :formatter="statusFormat"
-        width="100"
-      />
-      <el-table-column
-        label="创建者"
-        align="center"
-        prop="createBy"
-        width="100"
-      />
-      <el-table-column
-        label="创建时间"
-        align="center"
-        prop="createTime"
-        width="100"
-      >
+    <el-table v-loading="loading" :data="noticeList">
+      <el-table-column label="序号" align="center" prop="noticeId" width="100">
         <template slot-scope="scope">
-          <span>{{ parseTime(scope.row.createTime, "{y}-{m}-{d}") }}</span>
+          <div>
+            {{
+              scope.$index +
+              (queryParams.pageNum - 1) * queryParams.pageSize +
+              1
+            }}
+          </div>
+        </template>
+      </el-table-column>
+      <el-table-column label="商品名称" align="center" prop="spmc">
+        <template slot-scope="scope">
+          {{ scope.row.spmc }}
+          <span
+            class="copy"
+            @click="handelcopy"
+            v-if="scope.row.status == '已归档'"
+            >【复制】</span
+          >
+        </template>
+      </el-table-column>
+      <el-table-column
+        label="平台编码"
+        align="center"
+        prop="ptbm"
+        width="150"
+      />
+      <el-table-column
+        label="商品品牌"
+        align="center"
+        prop="sppp"
+        width="150"
+      />
+      <el-table-column
+        label="商品条码"
+        align="center"
+        prop="sptm"
+        width="150"
+      />
+      <el-table-column label="库存" align="center" prop="kc" width="150">
+        <template slot-scope="scope">
+          <el-tooltip placement="bottom" effect="light">
+            <div slot="content">
+              <div v-for="(item, index) in scope.row.kcsp" :key="index">
+                {{ item.name + item.type + item.size + item.num }}
+              </div>
+            </div>
+            <div class="stock">{{ scope.row.kc }}</div>
+          </el-tooltip>
         </template>
       </el-table-column>
       <el-table-column
         label="操作"
         align="center"
         class-name="small-padding fixed-width"
+        width="200"
       >
         <template slot-scope="scope">
           <el-button
             size="mini"
             type="text"
+            icon="el-icon-s-check"
+            @click="handleApproval(scope.row)"
+            v-hasPermi="['commodity:plaftorm:approval']"
+            >提交审核</el-button
+          >
+          <el-button
+            size="mini"
+            type="text"
             icon="el-icon-edit"
             @click="handleUpdate(scope.row)"
-            v-hasPermi="['system:notice:edit']"
+            v-hasPermi="['commodity:plaftorm:edit']"
             >修改</el-button
           >
           <el-button
@@ -171,7 +141,7 @@
             type="text"
             icon="el-icon-delete"
             @click="handleDelete(scope.row)"
-            v-hasPermi="['system:notice:remove']"
+            v-hasPermi="['commodity:plaftorm:del']"
             >删除</el-button
           >
         </template>
@@ -191,16 +161,17 @@
       <el-form ref="form" :model="form" :rules="rules" label-width="80px">
         <el-row>
           <el-col :span="12">
-            <el-form-item label="公告标题" prop="noticeTitle">
+            <el-form-item label="商品名称" prop="spmc">
               <el-input
-                v-model="form.noticeTitle"
-                placeholder="请输入公告标题"
+                v-model="form.spmc"
+                placeholder="请输入商品名称"
+                style="width:200px;"
               />
             </el-form-item>
           </el-col>
           <el-col :span="12">
             <el-form-item label="公告类型" prop="noticeType">
-              <el-select v-model="form.noticeType" placeholder="请选择">
+              <el-select v-model="form.noticeType" style="width:200px;" placeholder="请选择">
                 <el-option
                   v-for="dict in typeOptions"
                   :key="dict.dictValue"
@@ -245,7 +216,9 @@ import {
   addNotice,
   updateNotice,
   exportNotice,
-} from "@/api/system/notice";
+  approvalNotice,
+  copyNotice,
+} from "@/api/commodity/commodity";
 import Editor from "@/components/Editor";
 
 export default {
@@ -256,7 +229,7 @@ export default {
   data() {
     return {
       // 遮罩层
-      loading: true,
+      loading: false,
       // 选中数组
       ids: [],
       // 非单个禁用
@@ -268,7 +241,58 @@ export default {
       // 总条数
       total: 0,
       // 公告表格数据
-      noticeList: [],
+      noticeList: [
+        {
+          id: 1,
+          spmc: "薰衣草睡美人沐浴露",
+          status: "已归档",
+          ptbm: "52134864316",
+          sppp: "宝格丽",
+          sptm: "BGL205",
+          kc: "200",
+          kcsp: [
+            {
+              id: "1",
+              name: "薰衣草睡美人",
+              type: "沐浴露",
+              size: "260g",
+              num: "100",
+            },
+            {
+              id: "1",
+              name: "薰衣草睡美人",
+              type: "沐浴露",
+              size: "260g",
+              num: "100",
+            },
+          ],
+        },
+        {
+          id: 1,
+          spmc: "薰衣草睡美人沐浴露2",
+          status: "已归档",
+          ptbm: "52134864316",
+          sppp: "宝格丽2",
+          sptm: "BGL205",
+          kc: "200",
+          kcsp: [
+            {
+              id: "1",
+              name: "薰衣草睡美人",
+              type: "沐浴露",
+              size: "260g",
+              num: "100",
+            },
+            {
+              id: "1",
+              name: "薰衣草睡美人",
+              type: "沐浴露",
+              size: "260g",
+              num: "100",
+            },
+          ],
+        },
+      ],
       // 弹出层标题
       title: "",
       // 是否显示弹出层
@@ -284,13 +308,14 @@ export default {
         noticeTitle: undefined,
         createBy: undefined,
         status: undefined,
+        type: "0",
       },
       // 表单参数
       form: {},
       // 表单校验
       rules: {
-        noticeTitle: [
-          { required: true, message: "公告标题不能为空", trigger: "blur" },
+        spmc: [
+          { required: true, message: "商品名称不能为空", trigger: "blur" },
         ],
         noticeType: [
           { required: true, message: "公告类型不能为空", trigger: "change" },
@@ -300,7 +325,7 @@ export default {
     };
   },
   created() {
-    this.getList();
+    // this.getList();
     this.getDicts("sys_notice_status").then((response) => {
       this.statusOptions = response.data;
     });
@@ -309,7 +334,15 @@ export default {
     });
   },
   methods: {
+    /** 查询公告列表 */
+    handelcopy() {
+      copyNotice().then((res) => {
+        this.msgSuccess("复制成功");
+      });
+    },
+    /** 查询公告列表 */
     changeStatus(value) {
+      this.queryParams.pageNum = 1;
       this.getList();
     },
     /** 查询公告列表 */
@@ -367,15 +400,35 @@ export default {
       this.open = true;
       this.title = "添加公告";
     },
+    /** 审核按钮操作 */
+    handleApproval(row) {
+      const noticeId = row.noticeId || this.ids;
+      const noticeIds = row.noticeId || this.ids;
+      this.$confirm("是否确认审核该商品?", "警告", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning",
+      })
+        .then(function () {
+          return approvalNotice(noticeIds);
+        })
+        .then(() => {
+          this.getList();
+          this.msgSuccess("审核成功");
+        })
+        .catch(() => {});
+    },
     /** 修改按钮操作 */
     handleUpdate(row) {
       this.reset();
-      const noticeId = row.noticeId || this.ids;
-      getNotice(noticeId).then((response) => {
-        this.form = response.data;
-        this.open = true;
-        this.title = "修改公告";
-      });
+      this.title = "修改海关商品备案";
+      this.open = true;
+    //   const noticeId = row.noticeId || this.ids;
+    //   getNotice(noticeId).then((response) => {
+    //     this.form = response.data;
+    //     this.open = true;
+    //     this.title = "修改公告";
+    //   });
     },
     /** 提交按钮 */
     submitForm: function () {
@@ -400,15 +453,11 @@ export default {
     /** 删除按钮操作 */
     handleDelete(row) {
       const noticeIds = row.noticeId || this.ids;
-      this.$confirm(
-        '是否确认删除公告编号为"' + noticeIds + '"的数据项?',
-        "警告",
-        {
-          confirmButtonText: "确定",
-          cancelButtonText: "取消",
-          type: "warning",
-        }
-      )
+      this.$confirm("是否确认删除该商品?", "警告", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning",
+      })
         .then(function () {
           return delNotice(noticeIds);
         })
@@ -422,7 +471,14 @@ export default {
 };
 </script>
 <style scoped lang="scss">
-.radio-box{
-    margin-bottom:10px ;
+.radio-box {
+  margin-bottom: 10px;
+}
+.stock {
+  text-decoration: underline;
+}
+.copy {
+  color: #999;
+  cursor: pointer;
 }
 </style>
